de5997d263ccc4c2966f2221b32133ba21887aec
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-10-13 10:58:36 -0500

    accidently was adding commits without adding changes. see previous commits for notes

---
+++ states.py
@@ -0,0 +1,8 @@
+
+state_list = ["Wyoming", "West Virginia", "Wisconsin", "Washington", "Vermont", "Virginia", "Utah", "Texas", "Tennessee",
+              "South Dakota", "South Carolina", "Rhode Island", "Pennsylvania", "Oregon", "Oklahoma", "Ohio", "New York",
+              "Nevada", "New Mexico", "New Jersey", "New Hampshire", "Nebraska", "North Dakota", "North Carolina", "Montana",
+              "Mississippi", "Missouri", "Minnesota", "Michigan", "Maine", "Maryland", "Massachusetts", "Louisiana", "Kentucky",
+              "Kansas", "Indiana", "Illinois", "Idaho", "Iowa", "Hawaii","Georgia", "Florida", "Delaware", "District of Columbia",
+              "Connecticut", "Colorado", "California", "Arizona", "Arkansas", "Alabama", "Alaska",]
+
