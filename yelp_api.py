c8256b0691c26482f1d6fbe64e4d7fbad002fdb3
Alicia <alicia.nguyen@go.minneapolis.edu>
2020-10-16 00:42:34 -0700

    added comments and error handling

--- yelp_api.py
+++ yelp_api.py
@@ -2,22 +2,27 @@
 import os
 import requests
 
-YELP_API_KEY = os.environ.get('YELP_API_KEY')
+def get_user_feedback(): 
+    #checking user input
+    try:
+        feedback = ''
+        while not feedback:    
+            feedback = input('What type of resturants? ')
+            print(feedback)
+    except:
+        print(f'User entered incorrected information"{feedback}".')
 
 #fetching data from yelp location API
 yelp_url = 'https://api.yelp.com/v3/businesses/search'
-
-inquiry = input('What type of resturants? ')
-
-#Define the Header
-
+YELP_API_KEY = os.environ.get('YELP_API_KEY')
 headers = {'Authorization': 'Bearer' + YELP_API_KEY}
 parameters = {
-    'term' : inquiry,
+    'term' : feedback,
     'categories' : 'resturants',
     'location': 'Chicago,IL',
     'radius': '20000',
     'limit': 50}  
+
 #Make a request to the yelp API
 response = requests.get(yelp_url, headers=headers, params=parameters).json(0)
 resturants = response['businesses']
