85b451c54c97d95d624cec07f3bdedbf80a32b5e
Alicia <alicia.nguyen@go.minneapolis.edu>
2020-10-17 20:52:09 -0700

    fixed typo, parameter and removed blank spaces

--- yelp_api.py
+++ yelp_api.py
@@ -1,30 +1,33 @@
 #Import modules
-import os
 import requests
+
+import os
 
 #fetching data from yelp location API
 yelp_url = 'https://api.yelp.com/v3/businesses/search'
+
 
 #Get the key from the environment varialbles
 YELP_API_KEY = os.environ.get('YELP_API_KEY')
 
 def get_restaurants_for_location(location):
+    
+    headers = {'Authorization': 'Bearer %s' % YELP_API_KEY}
+    query_params =  {'term': YELP_API_KEY, 'businesses': location}
 
-    query_params =  {'access_key': YELP_API_KEY, 'businesses':location}
-        if query_params:
-            print(query_params)
-        
+        if query_params == 'term' and query_params == 'businesses':
+            print('Found')
         else:
-            print('not found') 
+            print('Not found') 
+    
     #Make a request to the yelp API
     #Convert JSON response to Python dictionary
-    response = requests.get(yelp_url, params=query_params).json()
-
+    response = requests.get(yelp_url, params=query_params, headers=headers).json()
     print(response)
 
-    resturants = response['businesses'] #results is a list 
+    restaurants = response['businesses'] #results is a list 
 
-    for rs in resturants:
+    for rs in restaurants:
         name = rs['name']
         rating = rs['rating']
         location = rs['location']
@@ -34,5 +37,5 @@ def get_restaurants_for_location(location):
 
 
 if __name__ == '__main__':
-    restaurants = get_restaurants_for_location('Chicago,IL') # change to different locations as needed 
+    restaurants = get_restaurants_for_location('Montana, MT') # change to different locations as needed 
     print(restaurants)
