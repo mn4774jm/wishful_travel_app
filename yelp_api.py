aa7cb0b87566ebe7c919ee1404fdabba2b9e89b0
Alicia <alicia.nguyen@go.minneapolis.edu>
2020-10-19 14:48:38 -0700

    add more parameters and try-except clause

--- yelp_api.py
+++ yelp_api.py
@@ -13,32 +13,39 @@ except KeyError:
     An environment variable called YELP_API_KEY must be set containing the location
     of the yelp api key""")
 
-def get_restaurants_for_location(location):
+def get_restaurants_for_location(term, location):
     
     headers = {'Authorization': 'Bearer %s' % YELP_API_KEY}
-    query_params =  {'term': 'restaurants', 'location': location}
+    query_params =  {'term': 'search_term','categories': category, 'location': location, 'radius': 10000, 'price': price, 'limit': 20}
 
-    if query_params == 'term' and query_params == 'businesses':
-        print('Found')
-    else:
-        print('Not found') 
+    #if query_params == 'term' and query_params == 'businesses':
+    #    print('Found')
+    #else:
+    #    print('Not found') 
 
     #Make a request to the yelp API
     #Convert JSON response to Python dictionary
-    response = requests.get(yelp_url, params=query_params, headers=headers).json()
-    print(response)
+    try:
+            response = requests.get(yelp_url, params=query_params, headers=headers).json()
+            print(response)
 
-    restaurants = response['businesses'] #results is a list 
+            restaurants = response['businesses'] #results is a list 
 
-    for rs in restaurants:
-        name = rs['name']
-        rating = rs['rating']
-        location = rs['location']
-        address =  ','.join(location['display_address'])
+                for r in restaurants:
+                name = r['name']
+                rating = r['rating']
+                location = r['location']
+                address =  ','.join(location['display_address'])
+            
+                print(f'{name}, {rating}, {address}')
 
-        print(f'{name}, {rating}, {address}')
+            return restaurants
+    
+    except AssertionError as e:
+        print('Request.get() function was not executed')
 
 
 if __name__ == '__main__':
-    restaurants = get_restaurants_for_location('Minneapolis, MN') # change to different locations as needed 
+    restaurants = get_restaurants_for_location('Minneapolis, MN) # change to different locations as needed 
     print(restaurants)
+    
