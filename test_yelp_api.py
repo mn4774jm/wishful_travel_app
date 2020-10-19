b682c3eb89ac92eb54de7d0c8f1b371ab1f7610c
Alicia <alicia.nguyen@go.minneapolis.edu>
2020-10-19 08:24:22 -0700

    add unittest.mock

---
+++ test_yelp_api.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest import TestCase
+from unittest.mock import patch
+
+import location
+
+class TestYelpApi(unittest.TestCase):
+
+    def test_get_search_location(self):
+        self.assertEqual('', location.yelp_api(''))# test empty string
+        self.assertEqual('Chicago', location.yelp_api('      Chicago     '))# test for extra spaces
+
+    @patch('yelp_api.get_restaurants_for_location', return_value='nowhere')
+    def test_get_no_restaurants_for_location(self, mock_get_restaurants_for_location):
+        yelp_api.zero_restaurants('', Minneapolis)
+        mock_get_restaurants_for_location.assert_showed()
+
+if __name__ == "__main__":
+     unittest.main()        
