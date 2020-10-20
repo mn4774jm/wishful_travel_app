0b3c8b2546f695b1199fabb5c4c263c8a3780e89
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-10-20 09:57:04 -0500

    fixed minor bug that was displaying the last state in the list in the dropdown as the default

--- states.py
+++ states.py
@@ -1,8 +1,15 @@
 
-state_list = ["Wyoming", "West Virginia", "Wisconsin", "Washington", "Vermont", "Virginia", "Utah", "Texas", "Tennessee",
-              "South Dakota", "South Carolina", "Rhode Island", "Pennsylvania", "Oregon", "Oklahoma", "Ohio", "New York",
-              "Nevada", "New Mexico", "New Jersey", "New Hampshire", "Nebraska", "North Dakota", "North Carolina", "Montana",
-              "Mississippi", "Missouri", "Minnesota", "Michigan", "Maine", "Maryland", "Massachusetts", "Louisiana", "Kentucky",
-              "Kansas", "Indiana", "Illinois", "Idaho", "Iowa", "Hawaii","Georgia", "Florida", "Delaware", "District of Columbia",
-              "Connecticut", "Colorado", "California", "Arizona", "Arkansas", "Alabama", "Alaska",]
+# state_list = ["Wyoming", "West Virginia", "Wisconsin", "Washington", "Vermont", "Virginia", "Utah", "Texas", "Tennessee",
+#               "South Dakota", "South Carolina", "Rhode Island", "Pennsylvania", "Oregon", "Oklahoma", "Ohio", "New York",
+#               "Nevada", "New Mexico", "New Jersey", "New Hampshire", "Nebraska", "North Dakota", "North Carolina", "Montana",
+#               "Mississippi", "Missouri", "Minnesota", "Michigan", "Maine", "Maryland", "Massachusetts", "Louisiana", "Kentucky",
+#               "Kansas", "Indiana", "Illinois", "Idaho", "Iowa", "Hawaii","Georgia", "Florida", "Delaware", "District of Columbia",
+#               "Connecticut", "Colorado", "California", "Arizona", "Arkansas", "Alabama", "Alaska",]
 
+state_list = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut",
+               "District of Columbia", "Delaware", "Florida", "Georgia", "Iowa", "Idaho", "Illinois", "Indiana",
+               "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota",
+               "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire",
+               "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
+               "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Vermont",
+               "Washington", "Wisconsin", "West Virginia", "Wyoming"]
