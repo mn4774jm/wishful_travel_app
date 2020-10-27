a801be48faebbcdd5aac56744122b56f0e8052ce
Alicia <alicia.nguyen@go.minneapolis.edu>
2020-10-27 13:17:35 -0700

    Clean up syntax issues and add more comments

--- db/yelpdb.py
+++ db/yelpdb.py
@@ -1,6 +1,5 @@
 import sqlite3
-#import datetime
-#import yelp_api
+import yelp_api
 
 db = 'yelp.DB_sqlite'
 #create yelp database table 
@@ -17,15 +16,30 @@ def create_table():
     conn.close()
 
 #query database to look up information on specific places
-def search_for_restaurants( location):
-    with sqlite3.connect(db) as conn:
-        conn.execute("SELECT * FROM yelp WHERE location=?", (location))
-        return conn.fetchall()
-    
-    #Add new city and state to yelp table
-def insert_info():
+def search_for_restaurants(name):
+    error = None
+    re = 'sushi'
+    if re == 'sushi':
+            with sqlite3.connect(db) as conn:
+                cur = conn.cursor()
+                try:
+                    data = conn.execute("SELECT name FROM yelp WHERE name= ?", (name),)
+                    cur.fetchall(data)    
+                        #return conn.fetchall()
+                    if data:
+                        return data
+                    else:
+                        print("Invalid search. Try again.", error)
+                except Exception as e:
+                    return(e) 
+            conn.close()        
+
+    #Insert new city and state to yelp table
+def insert_info(location):
      with sqlite3.connect(db) as conn:
-         conn.execute("INSERT INTO yelp values(80, 'Maine')")
+         conn.execute("INSERT INTO yelp (id, city, state, restaurant, name, rating, created) VALUES (?, ? , ? , ?, ?, ?, ?", (
+          location.id, location.city, location.state, location.restaurant, location.name, location.rating, location.created))
+     
      conn.close()
 
     #Show all data by row in yelp table
@@ -71,7 +85,7 @@ def delete_state(state):
     conn.close()
 
 create_table()
-search_for_restaurants('sushi')
+search_for_restaurants(name='sushi')
 insert_info()
 display_all_data()
 display_one_location('Chicago')
