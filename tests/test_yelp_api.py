7825a3b683edd6c1ed9530e806a5e2ba2edf8201
Alicia <alicia.nguyen@go.minneapolis.edu>
2020-10-27 19:16:55 -0700

    Add more testings

--- tests/test_yelp_api.py
+++ tests/test_yelp_api.py
@@ -1,25 +1,29 @@
 import unittest
 from unittest import TestCase
 from unittest.mock import patch
-
+import yelp
 import requests
-import yelp_api
 
 class YelpapiTest(unittest.TestCase):
 
     def setUp(self):
         self.a = 'state'
         self.b = 'city'
-
-         """ Testing  get restaurants() in retrieving location"""    
-    def test_get_location(self):
+ 
+    def test_get_location(self): #Testing  get restaurants() in retrieving location    
         #Arrange
         print("Test 1: ")
         #Action
         result = requests.get(self.a, self.b)
         #Assert
         self.assertEqual(result, self.a, self.b)
-
+    
+    """Checking yelp api for no locations"""
+    @patch('yelp_api.get_restaurants_for_location', return_value='nowhere')
+    def test_get_no_restaurants_for_location(self, mock_get_restaurants_for_location):
+        n_location = yelp.zero_restaurants('  ')
+        mock_get_restaurants_for_location.assert_showed()
+        print(n_location)
              
     if __name__ == "__main__":
      unittest.main()            
