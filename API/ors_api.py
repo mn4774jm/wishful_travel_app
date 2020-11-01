ee5d7997b0213196c053c4c88817e64363a805a9
Jordan Hughes <ij5775wy@go.minneapolis.edu>
2020-10-31 19:36:21 -0500

    Moved useful code to the top, so you don't need to dig through retired code to get to what you need.

--- API/ors_api.py
+++ API/ors_api.py
@@ -18,6 +18,46 @@ what is still used.
 ors_key = '5b3ce3597851110001cf6248701a505f4a83471da001d266b2860ac6'
 # country = 'usa'
 
+# Uses coordinates for both the beginning and ending locations to retreive directions to travel from point A to point B.
+def get_directions(end):
+
+    # Catches error 403 if key is missing and if it's attempted to be obtained with an environment variable
+    if ors_key is None:
+        return 'Openrouteservice API key missing', None
+
+    # parameter structure - key, start (long, lat), end (long, lat)
+    directions_URL = ('https://api.openrouteservice.org/v2/directions/driving-car')
+
+    start = '-93.278494, 44.941691'
+
+    try:
+        query_parameters = {'api_key': ors_key, 'start': start, 'end': end}
+
+        try:
+            query = requests.get(directions_URL, params=query_parameters).json()
+
+        # Will likely catch more than error 503, but at least I can feel fairly confident that it'll catch error 503 
+        # (can't specifically test for 503 because that would require the ors servers to be offline, but works when the user if offline)
+        except:
+            return 'Could not contact openrouteservice API', None
+
+        steps = query['features'][0]['properties']['segments'][0]['steps']
+        return None, steps
+
+    # Catches error 400 and, if the key is incorrect, 403
+    except KeyError:
+        return 'Unable to create directions', None
+
+    # Tried to catch error 503 here, but couldn't find any applicable code/keywords to use
+    # Moved this try/except inside the first try block
+    # except:
+    #     return 'Could not contact openrouteservice API', None
+
+    # Catches any miscellaneous errors that somehow got past the first two APIs
+    except Exception as e:
+        logging.error(e)
+        return 'An unexpected error has been encountered', None
+
 """
 
 # This method uses the Structured Forward Grocode Service to get the general area of whatever is being searched for, hopefully
@@ -59,62 +99,14 @@ ors_key = '5b3ce3597851110001cf6248701a505f4a83471da001d266b2860ac6'
     except:
         print('Error: Location not found')
 
-"""
-
-# Uses coordinates for both the beginning and ending locations to retreive directions to travel from point A to point B.
-def get_directions(end):
-
-    # Catches error 403 if key is missing and if it's attempted to be obtained with an environment variable
-    if ors_key is None:
-        return 'Openrouteservice API key missing', None
-
-    # parameter structure - key, start (long, lat), end (long, lat)
-    directions_URL = ('https://api.openrouteservice.org/v2/directions/driving-car')
-
-    start = '-93.278494, 44.941691'
-
-    try:
-        query_parameters = {'api_key': ors_key, 'start': start, 'end': end}
-
-        try:
-            query = requests.get(directions_URL, params=query_parameters).json()
-
-        # Will likely catch more than error 503, but at least I can feel fairly confident that it'll catch error 503 
-        # (can't specifically test for 503 because that would require the ors servers to be offline, but works when the user if offline)
-        except:
-            return 'Could not contact openrouteservice API', None
-
-        steps = query['features'][0]['properties']['segments'][0]['steps']
-        return None, steps
-
-"""
-
-        Old for loop that was used to walk through each step of directions
-        count = 1
-        for s in steps:
-            # Convert distances from meters to miles
-            distance = round((s['distance']*3.28)/5280, 2)
-            direction = s['instruction']
-            print(f'{count}. {direction} | {distance} miles.')
-            count += 1
-
-"""
-
-    # Catches error 400 and, if the key is incorrect, 403
-    except KeyError:
-        return 'Unable to create directions', None
-
-    # Tried to catch error 503 here, but couldn't find any applicable code/keywords to use
-    # Moved this try/except inside the first try block
-    # except:
-    #     return 'Could not contact openrouteservice API', None
-
-    # Catches any miscellaneous errors that somehow got past the first two APIs
-    except Exception as e:
-        logging.error(e)
-        return 'An unexpected error has been encountered', None
-
-"""
+Old for loop that was used to walk through each step of directions
+count = 1
+for s in steps:
+    # Convert distances from meters to miles
+    distance = round((s['distance']*3.28)/5280, 2)
+    direction = s['instruction']
+    print(f'{count}. {direction} | {distance} miles.')
+    count += 1
 
 Old code that was used to test the program while working on it
 Now all that's needed is the endpoint (longitude, latitude) for get_directions
