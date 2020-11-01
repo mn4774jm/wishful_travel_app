f3dcd740e50291f224659b947a31fde0069f1f9b
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-10-31 19:46:38 -0500

    various comments added

--- managers/manager_cache.py
+++ managers/manager_cache.py
@@ -3,7 +3,8 @@ from db_calls import get_data_from_cache
 from utility_functions import restaurant_formatter, direction_formatting
 from API.wiki_api import get_page_url
 
-
+# Cache manager is responsible for retrieving data from the cache to either be used to populate the renders or be
+# added to the dictionary
 def cache_manager(city, state):
     page_id, page_data = convert_data_wiki(get_data_from_cache(city, state, 'wiki'))
     session_url = get_page_url(page_id)
