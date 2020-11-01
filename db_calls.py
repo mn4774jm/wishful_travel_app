8e2a3f4e198ca79a938cf82eae0f70427472b997
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-10-31 23:55:25 -0500

    change to db_calls that was causing bookmarks to not be retrieved correctly.

--- db_calls.py
+++ db_calls.py
@@ -7,29 +7,35 @@ import time
 
 # TODO exception handling and comments
 
-
+# method to delete rows from the cache after a designated point in time. The expiry field in the cache table is the
+# integer UTC value for the date the row was created plus 30 days. If that value is lower than the current, it is
+# deleted
 def check_expire_for_cache():
     with sqlite3.connect(db_path) as conn:
         dates_to_delete = conn.execute("DELETE FROM cache WHERE ? > expiry_date", (time.time(),))
         print(dates_to_delete)
 
 
+# simple query to check if city already exists in the cache to avoid making api requests more than is required
 def search_for_city_in_cache(city, state):
     with sqlite3.connect(db_path) as conn:
         data = conn.execute("SELECT * FROM cache WHERE city= ? and state = ?", (city, state))
         return data.fetchone()
 
 
+# query to check if new row already exists in the DB before attempting to create a new row in the table
 def search_bookmark_exists(city, state):
     with sqlite3.connect(db_path) as conn:
         data = conn.execute("SELECT * FROM bookmarks WHERE city= ? and state = ?", (city, state))
         return data.fetchone()
 
 
+# data added to the new row includes an integer for UTC time plus an additional 30 days for the expiration date
 def add_to_cached_data(api_name, city, state, data):
     with sqlite3.connect(db_path) as conn:
         conn.execute('insert into cache values (NULL,?,?, ?, ?,?)', (api_name, city, state, data, time.time() + 43200))
         conn.commit()
+
 
 # TODO table not returning data when state argument is added to query
 def get_data_from_cache(city, state, api):
@@ -44,7 +50,7 @@ def add_to_bookmarks(city, state, wiki_entry, restaurants, directions, wiki_url)
             conn.execute('insert into bookmarks values (NULL,?,?,?,?,?,?,?)',
                          (city, state, wiki_entry, restaurants, directions, wiki_url, datetime.now()))
             conn.commit()
-        except Exception as e:
+        except ValueError as e:
             return 'Unable to commit to database'
 
 
